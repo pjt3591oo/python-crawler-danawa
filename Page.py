b9712e251613dc05c0c3284095e0ce858dbab5ab
pjt3591oo <pjt3591oo@gmail.com>
2016-12-10 20:16:30 +0900

    제품 스펙 추가, 가격 수정

--- Page.py
+++ Page.py
@@ -6,7 +6,8 @@ class Page:
     product_tag = '.prod_main_info'
     thumbnail_img_tag = '.thumb_image img'
     name_tag = 'p.prod_name a'
-    price_tag = '.prod_pricelist p.price_sect a'
+    spec_tag = '.prod_spec_set .spec_list a'
+    price_tag = '.prod_pricelist p.price_sect strong'
 
     def __init__(self, cate_list, browser):
         self.cate_list = cate_list
@@ -14,6 +15,7 @@ class Page:
 
     def page_crawler(self):
         for url in self.cate_list:
+
             self.browser.get(url["url"])
             self.browser.find_element_by_css_selector(self.name_tag)
             soup = BeautifulSoup(self.browser.page_source, 'lxml')
@@ -29,13 +31,21 @@ class Page:
                 for product in soup.select(self.product_tag):
                     print(product.select(self.thumbnail_img_tag)[0].get('src'))
                     print(product.select(self.name_tag)[0].text.strip().replace('\u2013', ''))
+                    print(self.get_product_spec_info(product))
                     print(product.select(self.price_tag)[0].text.strip().replace('\u2013', ''))
-                    time.sleep(0.5)
+
+                time.sleep(1)
 
                 page_count += 1
 
     def next_page(self, pagination):
         self.browser.execute_script('movePage(' + str(pagination) + ')')
+
+    def get_product_spec_info(self, product):
+        spec = ''
+        for i in product.select(self.spec_tag):
+            spec += i.text.replace('\u2022', '').replace('\ufffd', '') + '/'
+        return spec
 
     @staticmethod
     def __is_last_page(product):
